50e9a6cfe2091aa7fa38e96c98d04bc116165abc
aumermeak <mik@Mik.localdomain>
2025-09-05 19:21:26 +0800

    added new modernized arrow up button

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -397,9 +397,14 @@ onMounted(() => {
 
         </section>
         <!-- Back to Top Button -->
-        <button v-show="showButton" @click="scrollToTop"
-            class="fixed bottom-6 right-6 p-3 rounded-full bg-green-600 text-white shadow-lg hover:bg-green-700 transition">
-            ⬆️
+        <button v-show="showButton" @click="scrollToTop" class="fixed bottom-6 right-6 w-12 h-12 flex items-center justify-center rounded-full 
+           bg-gradient-to-r from-green-500 to-emerald-600 text-white shadow-lg 
+           hover:scale-110 hover:shadow-xl transition-transform duration-300 ease-out">
+            <!-- Up Arrow Icon -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
+            </svg>
         </button>
         <transition enter-active-class="transition transform duration-700"
             enter-from-class="opacity-0 translate-y-10 scale-90" enter-to-class="opacity-100 translate-y-0 scale-100">
